8bd0f55daac23be76bbd204c316fd57bdc70ebcf
Rishabh Mehrotra <mehrotra.rishab@gmail.com>
2020-08-31 13:10:54 +0530

    custom errors made and handled async errors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,24 +1,30 @@
-import express from 'express'
-import { json } from 'body-parser'
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
 
-import { currentUserRouter } from './routes/current-user'
-import { signinRouter } from './routes/signin'
-import { signoutRouter } from './routes/signout'
-import { signupRouter } from './routes/signup'
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
 
-import { errorHandler } from './middlewares/error-handler'
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
 
-app.use(currentUserRouter)
-app.use(signinRouter)
-app.use(signoutRouter)
-app.use(signupRouter)
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
-app.use(errorHandler)
+app.use(errorHandler);
 
+
+app.all('*', async () => {
+    throw new NotFoundError();
+})
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000!!!!s')
+    console.log('Listening on port 3000!!!!');
 })
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,7 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { CustomError } from '../errors/custom-error';
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.log('Something went wrong', err);
-
-    res.status(400).send({ message: err.message})
+    if (err instanceof CustomError) {
+        return res.status(err.statusCode).send({ errors: err.serializeErrors() })
+    }
+    res.status(400).send({ message: err.message })
 };
